35e07f6d96aac1574da27f52a9e4134985bacf08
alish <alishojib12@gmail.com>
2022-10-17 01:37:26 +0600

    code commet

--- Series2/src/series2/Series2.java
+++ Series2/src/series2/Series2.java
@@ -1,25 +1,27 @@
-package series2;
-
-import java.util.Scanner;
-
-public class Series2 {
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n, multiplication =1;
-
-        System.out.print("Enter The Last Number = ");
-        n = input.nextInt();
-        
-        for (int i = 1; i <=n ; i=i+2) {
-            System.out.print(i+" ");
-            multiplication=multiplication*i;
-            
-        }
-        System.out.println("");
-        System.out.println(multiplication);
-    }
-    
-    
-    }    
-
+/*
+ * package series2;
+ * 
+ * import java.util.Scanner;
+ * 
+ * public class Series2 {
+ * 
+ * public static void main(String[] args) {
+ * Scanner input = new Scanner(System.in);
+ * int n, multiplication =1;
+ * 
+ * System.out.print("Enter The Last Number = ");
+ * n = input.nextInt();
+ * 
+ * for (int i = 1; i <=n ; i=i+2) {
+ * System.out.print(i+" ");
+ * multiplication=multiplication*i;
+ * 
+ * }
+ * System.out.println("");
+ * System.out.println(multiplication);
+ * }
+ * 
+ * 
+ * }
+ * 
+ */
--- Series2/src/series2/powermathmatics.java
+++ Series2/src/series2/powermathmatics.java
@@ -1,23 +1,25 @@
+
 package series2;
 
 import java.util.Scanner;
 
 public class powermathmatics {
+
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
-        int n, sum=0;
-        
+        int n, sum = 0;
+
         System.out.print("Enter The Last Number = ");
         n = input.nextInt();
-        
-        for (int i = 1; i <=n; i++) {
-            System.out.print(i+"X"+i+" ");
-            sum = sum+i*i;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + "X" + i + " ");
+            sum = sum + i * i;
         }
         System.out.println("");
         System.out.println(sum);
-        
+
     }
-    
+
 }
